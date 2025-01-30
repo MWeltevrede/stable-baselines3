70d42043418cf3b746211dd60cb872792106b215
Max Weltevrede <maxweltevrede@gmail.com>
2025-01-30 11:56:12 +0100

    Add asynchronous rollout buffer

--- stable_baselines3/common/buffers.py
+++ stable_baselines3/common/buffers.py
@@ -615,6 +615,188 @@ class RolloutBuffer(BaseBuffer):
             self.returns[batch_inds].flatten(),
         )
         return RolloutBufferSamples(*tuple(map(self.to_torch, data)))
+    
+
+class AsyncRolloutBuffer(RolloutBuffer):
+    """
+    Rollout buffer used in on-policy algorithms like A2C/PPO.
+    It corresponds to ``buffer_size`` transitions collected
+    using the current policy.
+    This experience will be discarded after the policy update.
+    In order to use PPO objective, we also store the current value of each state
+    and the log probability of each taken action.
+
+    The term rollout here refers to the model-free notion and should not
+    be used with the concept of rollout used in model-based RL or planning.
+    Hence, it is only involved in policy and value function training but not action selection.
+
+    :param buffer_size: Max number of element in the buffer
+    :param observation_space: Observation space
+    :param action_space: Action space
+    :param device: PyTorch device
+    :param gae_lambda: Factor for trade-off of bias vs variance for Generalized Advantage Estimator
+        Equivalent to classic advantage when set to 1.
+    :param gamma: Discount factor
+    :param n_envs: Number of parallel environments
+    """
+
+    observations: np.ndarray
+    actions: np.ndarray
+    rewards: np.ndarray
+    advantages: np.ndarray
+    returns: np.ndarray
+    episode_starts: np.ndarray
+    log_probs: np.ndarray
+    values: np.ndarray
+
+    def __init__(
+        self,
+        buffer_size: int,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        device: Union[th.device, str] = "auto",
+        gae_lambda: float = 1,
+        gamma: float = 0.99,
+        n_envs: int = 1,
+    ):
+        super().__init__(buffer_size*10, observation_space, action_space, device, gae_lambda, gamma, n_envs=n_envs)
+        self.steps_to_collect = buffer_size
+
+    def reset(self) -> None:
+        self.indices = np.zeros(self.n_envs, dtype=np.uint64)
+        self.last_values = np.zeros(self.n_envs, dtype=np.float32)
+        self.last_dones = np.zeros(self.n_envs, dtype=np.float32)
+        super().reset()
+
+    def add(
+        self,
+        obs: np.ndarray,
+        action: np.ndarray,
+        reward: np.ndarray,
+        episode_start: np.ndarray,
+        value: th.Tensor,
+        log_prob: th.Tensor,
+        indices_to_add: np.ndarray,
+    ) -> None:
+        """
+        :param obs: Observation
+        :param action: Action
+        :param reward:
+        :param episode_start: Start of episode signal.
+        :param value: estimated value of the current state
+            following the current policy.
+        :param log_prob: log probability of the action
+            following the current policy.
+        """
+        # Don't add anything if that env dimension is already full
+        masked_indices_to_add = indices_to_add * (self.indices < self.buffer_size)
+        # But store the last value and done flags if the dimension was just filled
+        last_indices_to_add = indices_to_add * (self.indices == self.buffer_size)
+        self.last_values[last_indices_to_add] = value[last_indices_to_add].clone().cpu().numpy().flatten()
+        self.last_dones[last_indices_to_add] = np.array(episode_start[last_indices_to_add])
+        self.indices[last_indices_to_add] += 1
+
+        if len(log_prob.shape) == 0:
+            # Reshape 0-d tensor to avoid error
+            log_prob = log_prob.reshape(-1, 1)
+
+        # Reshape needed when using multiple envs with discrete observations
+        # as numpy cannot broadcast (n_discrete,) to (n_discrete, 1)
+        if isinstance(self.observation_space, spaces.Discrete):
+            obs = obs.reshape((self.n_envs, *self.obs_shape))
+
+        # Reshape to handle multi-dim and discrete action spaces, see GH #970 #1392
+        action = action.reshape((self.n_envs, self.action_dim))
+
+        column_vector = np.arange(self.n_envs, dtype=np.int64)[masked_indices_to_add]
+        pos = self.indices[masked_indices_to_add]
+        self.observations[pos, column_vector] = np.array(obs[masked_indices_to_add])
+        self.actions[pos, column_vector] = np.array(action[masked_indices_to_add])
+        self.rewards[pos, column_vector] = np.array(reward[masked_indices_to_add])
+        self.episode_starts[pos, column_vector] = np.array(episode_start[masked_indices_to_add])
+        self.values[pos, column_vector] = value[masked_indices_to_add].clone().cpu().numpy().flatten()
+        self.log_probs[pos, column_vector] = log_prob[masked_indices_to_add].clone().cpu().numpy()
+        self.indices[masked_indices_to_add] += 1
+
+        if np.any(self.indices >= self.buffer_size) or sum(self.indices) > self.n_envs*self.steps_to_collect:
+            self.full = True
+
+    def compute_returns_and_advantage(self, last_values: th.Tensor, dones: np.ndarray) -> None:
+        """
+        Post-processing step: compute the lambda-return (TD(lambda) estimate)
+        and GAE(lambda) advantage.
+
+        Uses Generalized Advantage Estimation (https://arxiv.org/abs/1506.02438)
+        to compute the advantage. To obtain Monte-Carlo advantage estimate (A(s) = R - V(S))
+        where R is the sum of discounted reward with value bootstrap
+        (because we don't always have full episode), set ``gae_lambda=1.0`` during initialization.
+
+        The TD(lambda) estimator has also two special cases:
+        - TD(1) is Monte-Carlo estimate (sum of discounted rewards)
+        - TD(0) is one-step estimate with bootstrapping (r_t + gamma * v(s_{t+1}))
+
+        For more information, see discussion in https://github.com/DLR-RM/stable-baselines3/pull/375.
+
+        :param last_values: state value estimation for the last step (one for each env)
+        :param dones: if the last step was a terminal step (one bool for each env).
+        """
+        # Convert to numpy
+        last_values = last_values.clone().cpu().numpy().flatten()  # type: ignore[assignment]
+
+        # Replace last_values and dones for env dimensions we already received earlier
+        last_indices_to_replace = self.indices == (self.buffer_size + 1)
+        last_values[last_indices_to_replace] = self.last_values[last_indices_to_replace]
+        dones[last_indices_to_replace] = self.last_dones[last_indices_to_replace]
+
+        for i in range(self.n_envs):
+            last_gae_lam = 0
+            for step in reversed(range(self.indices[i])):
+                if step == self.indices[i] - 1:
+                    next_non_terminal = 1.0 - dones[i].astype(np.float32)
+                    next_values = last_values[i]
+                else:
+                    next_non_terminal = 1.0 - self.episode_starts[step + 1][i]
+                    next_values = self.values[step + 1][i]
+                delta = self.rewards[step][i] + self.gamma * next_values * next_non_terminal - self.values[step][i]
+                last_gae_lam = delta + self.gamma * self.gae_lambda * next_non_terminal * last_gae_lam
+                self.advantages[step][i] = last_gae_lam
+            # TD(lambda) estimator, see Github PR #375 or "Telescoping in TD(lambda)"
+            # in David Silver Lecture 4: https://www.youtube.com/watch?v=PnHCvfgC_ZA
+        self.returns = self.advantages + self.values
+
+    def get(self, batch_size: Optional[int] = None) -> Generator[RolloutBufferSamples, None, None]:
+        assert self.full, ""
+        # indices = np.random.permutation(self.buffer_size * self.n_envs)
+        indices_to_sample = []
+        for i in range(self.n_envs):
+            step = self.indices[i]
+            for index_to_sample in np.arange(0, step):
+                indices_to_sample.append(self.buffer_size*i + index_to_sample)
+        indices_to_sample = np.random.permutation(np.array(indices_to_sample, dtype=np.int64))
+
+        # Prepare the data
+        if not self.generator_ready:
+            _tensor_names = [
+                "observations",
+                "actions",
+                "values",
+                "log_probs",
+                "advantages",
+                "returns",
+            ]
+
+            for tensor in _tensor_names:
+                self.__dict__[tensor] = self.swap_and_flatten(self.__dict__[tensor])
+            self.generator_ready = True
+
+        # Return everything, don't create minibatches
+        if batch_size is None:
+            batch_size = len(indices_to_sample)
+
+        start_idx = 0
+        while start_idx < len(indices_to_sample):
+            yield self._get_samples(indices_to_sample[start_idx : start_idx + batch_size])
+            start_idx += batch_size
 
 
 class DictReplayBuffer(ReplayBuffer):
